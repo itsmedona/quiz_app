47dc73a5519febfb8cb16dc597a6fda4c89adb2a
itsmedona <donashaji254@gmail.com>
2023-11-23 23:27:28 +0530

    nsxt commit

--- lib/view/HomePage/HomePage.dart
+++ lib/view/HomePage/HomePage.dart
@@ -85,3 +85,78 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+/*
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int? checkvalue;
+  int value = 0;
+
+  void counter() {
+    value = value + 1;
+    setState(() {});
+    print(value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+                "what programming language is primarily used for building flutter apps?"),
+            SizedBox(
+              height: 50,
+            ),
+            ListView.separated(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: 4,
+                itemBuilder: (context, index) => InkWell(
+                      onTap: () {
+                        // print(index);
+                        checkvalue = index;
+                        setState(() {});
+                      },
+                      child: Container(
+                        height: 50,
+                        color: checkvalue == index
+                            ? checkvalue == DataBase.myData[index]['answer']
+                                ? Colors.green
+                                : Colors.red
+                            : Colors.amber,
+                        child: Center(
+                            child: Text(DataBase.myData[0]['options'][index])),
+                      ),
+                    ),
+                separatorBuilder: (context, index) => SizedBox(
+                      height: 10,
+                    )),
+            Text(value.toString()),
+            SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  counter();
+                },
+                child: Text("save"))
+          ],
+        ),
+      ),
+    );
+  }
+}
+*/
--- lib/view/ResultPage/ResultPage.dart
+++ lib/view/ResultPage/ResultPage.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
 class ResultPage extends StatelessWidget {
-  const ResultPage({super.key, required int score}) 
-  
- // int score;
+  const ResultPage({super.key, required this.score});
+  final int score;
+
+  // int score;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("QwizB"),
+      ),
+      body: Center(
+        child: Text(score.toString()),
+      ),
+    );
   }
 }
